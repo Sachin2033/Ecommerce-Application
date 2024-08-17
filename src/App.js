d3ea3dbbed11b2940220954cd4ea1064f4d74f91
Sachin2033 <sachin96.pachouri@gmail.com>
2024-08-17 07:21:17 +0530

    Wroking on frontend Part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './customer/component/Footer/Footer';
+import Navigation from './customer/component/Navigation/Navigation';
+import Product from './customer/component/Product/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div>
+        {/* <HomePage /> */}
+        <Product />
+      </div>
+      <Footer />
     </div>
   );
 }
