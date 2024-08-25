75101b52227d5a9e24bbd6823abd2c8e44838371
Sachin2033 <89439026+Sachin2033@users.noreply.github.com>
2024-08-25 22:31:01 +0530

    Delete backend directory

--- backend/src/main/java/com/ecommerce/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.ecommerce.config;
-
-public class JwtConstant {
-	public static final String SECRET_KEY="jnfeijlwufiwufniwuqfnwiuqfbiuwoeihwebfwiyfbwifbuwf";
-	public static final String JWT_HEADER="Authorization";
-}
